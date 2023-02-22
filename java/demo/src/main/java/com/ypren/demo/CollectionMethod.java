ba1805e425ba9f88b762566ccaa7ce07459cf077
Yongping Ren <yongping@qti.qualcomm.com>
2023-02-22 16:42:14 +0800

    java: collection factory method

---
+++ java/demo/src/main/java/com/ypren/demo/CollectionMethod.java
@@ -0,0 +1,16 @@
+package com.ypren.demo;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Test Collection method.
+ */
+public class CollectionMethod {
+    public static void main(String[] args) {
+        List list = List.of("Apple", "Banana", "Strawberry");
+        Set set = Set.of("Egg", "Cake");
+        System.out.println(list);
+        System.out.println(set);
+    }
+}
